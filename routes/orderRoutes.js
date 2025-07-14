3f28cc2e266e372d35a223b3497895f43702c4c6
Bishnu <bishnubudathoki717@gmail.com>
2025-07-14 22:46:00 +0545

    order route

---
+++ routes/orderRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const orderCtrl = require("../../controllers/orderController");
+
+router.post("/", orderCtrl.createOrder);
+router.get("/", orderCtrl.getAllOrders);
+router.get("/user/:userId", orderCtrl.getOrdersByUser);
+router.patch("/:orderId", orderCtrl.updateOrderStatus);
+router.delete("/:orderId", orderCtrl.deleteOrder);
+
+module.exports = router;
